4364e6bcea533d686c6bce849f2c85d98bd3c857
leesiwoong <83443857+silverttthin@users.noreply.github.com>
2021-12-30 21:10:01 +0900

    123

--- study_record/6차시/49_Group_Anagrams.py
+++ study_record/6차시/49_Group_Anagrams.py
@@ -7,13 +7,18 @@ def groupAnagrams(strs):
     for word in strs : 
         
         sorted_word = ''.join(sorted(word)) # sorted 내장함수는 정렬 결과를 리스트로 줌
-        tmp[sorted_word].append(word)
+        if sorted_word not in tmp : # 정렬 결과가 같은게 없으면 새로운 리스트 만들어서 해당 word를 넣기
+            tmp[sorted_word] = []
+            tmp[sorted_word].append(word)
+            
+        else : # 정렬 결과가 같은게 있으면 해당 word를 같은 그룹에 넣기
+            tmp[sorted_word].append(word)
         
     ans = []
     for key in tmp:
         ans.append(tmp[key])
-
-    print(ans)
+    # print(ans)
+    return ans
 
 
 
